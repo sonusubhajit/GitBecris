621ec5c1d187c72e3582cfa23cbb4d6f9bd437b2
sonusubhajit <karmakarsubhajit123@gmail.com>
2023-06-02 14:21:01 +0530

    changes done (#1)

--- GitBecris/src/main/java/com/subhajit/gitbecris/GitBecris/controller/BecrisController.java
+++ GitBecris/src/main/java/com/subhajit/gitbecris/GitBecris/controller/BecrisController.java
@@ -2,9 +2,10 @@ package com.subhajit.gitbecris.GitBecris.controller;
 
 public class BecrisController {
 
+	private int i;
 	private int j;
 
 	public void test() {
-		System.out.print("I am subhajitt");
+		System.out.print("I am subhajitt"); 
 	}
 }
