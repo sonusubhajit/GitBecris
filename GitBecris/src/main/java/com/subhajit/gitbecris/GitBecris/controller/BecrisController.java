d429a9261fa239a74944e96c32be6ffd49dbb5f6
subhajit karmakar <karmakarsubhajit123@gmail.com>
2023-06-02 11:42:46 +0530

    cha

--- GitBecris/src/main/java/com/subhajit/gitbecris/GitBecris/controller/BecrisController.java
+++ GitBecris/src/main/java/com/subhajit/gitbecris/GitBecris/controller/BecrisController.java
@@ -3,6 +3,6 @@ package com.subhajit.gitbecris.GitBecris.controller;
 public class BecrisController {
 
 	public void test() {
-		System.out.print("I am subhajit");
+		System.out.print("I am subhajitt");
 	}
 }
